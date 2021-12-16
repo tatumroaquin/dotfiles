6b196601ee6f6060dbb8ae816b354a759df6a210
ormux <ormuseldesu@gmail.com>
2021-12-16 20:13:29 +1100

    deviation reduced to 2%, add dunst notification to show backlight level

--- .local/bin/backlight.sh
+++ .local/bin/backlight.sh
@@ -6,9 +6,15 @@ param=$1
 
 case $param in
    up)
-      xbacklight -inc 5
+      xbacklight -inc 2
+      dunstctl close 0
+      level=`printf "%0.0f" $(xbacklight -get)`
+      notify-send -u low "backlight" "increased to $level"
       ;;
    down)
-      xbacklight -dec 5
+      xbacklight -dec 2
+      dunstctl close 0
+      level=`printf "%0.0f" $(xbacklight -get)`
+      notify-send -u low "backlight" "decreased to $level"
       ;;
 esac
