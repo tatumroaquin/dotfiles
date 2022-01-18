07734c5dec68b420a384e901742c804d10b4cd10
ormux <ormuseldesu@gmail.com>
2022-01-18 18:25:04 +1100

    change property id from int to string

--- .local/bin/touchpad.sh
+++ .local/bin/touchpad.sh
@@ -1,4 +1,4 @@
 #!/bin/env zsh
 
-xinput set-prop "SynPS/2 Synaptics TouchPad" 320 1
-xinput set-prop "SynPS/2 Synaptics TouchPad" 328 1
+xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
+xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
