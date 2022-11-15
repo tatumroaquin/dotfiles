ea3e3b0be3302991609507ce532e3954bb1c143e
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2022-11-16 00:24:33 +1100

    update nvm-sh version, and change shell to bash, according to the maintainer's comments

--- .local/bin/install-nvm.sh
+++ .local/bin/install-nvm.sh
@@ -1,4 +1,4 @@
-#!/bin/env zsh
+#!/bin/sh
 export NVM_DIR="$XDG_DATA_HOME/nvm"
 export NVM_BIN="$XDG_DATA_HOME/node"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | zsh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
