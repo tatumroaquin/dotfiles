ee0c0198389d310705af737cac2f9cf42f360f51
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2022-12-16 12:01:52 +1100

    restore audio backlight controls

--- .local/bin/backlight.sh
+++ .local/bin/backlight.sh
@@ -1,30 +1,43 @@
-#!/bin/env zsh
+#!/bin/sh
 
 # script to adjust screen backlight using F5 and F6 fn keys
+# dependencies: pacman -S bc
 
 param=$1
+appname="light"
+tagname="brightness"
 
 increase() {
-   num=`xbacklight -get`
-   xbacklight -set $((num*1.5))
+   num=`printf "%0.0f" $(light -G)`
+   add=`printf "%0.0f" $(echo "$num * 1.5" | bc)`
+   light -S $add
 }
 
 decrease() {
-   num=`xbacklight -get`
-   xbacklight -set $((num/1.5))
+   #num=`xbacklight -get`
+   #xbacklight -set `echo $num/1.5 | bc`
+
+   num=`printf "%0.0f" $(light -G)`
+   sub=`printf "%0.0f" $(echo "$num / 1.5" | bc)`
+   [ ${num%.*} -le 2 ] && sub=$num && return
+   light -S $sub
+}
+
+notify () {
+  notify-send -a $appname -u $1 -i $2 \
+    -h string:x-dunst-stack-tag:$tagname \
+    -h int:value:"$4" "$3: $4%"
 }
 
 case $param in
    up)
       increase
-      dunstctl close 0
-      level=`printf "%0.0f" $(xbacklight -get)`
-      notify-send -u low "backlight" "increased to $level"
+      level=`printf "%0.0f" $(light -G)`
+      notify low computer-laptop brightness $level
       ;;
    down)
       decrease
-      dunstctl close 0
-      level=`printf "%0.0f" $(xbacklight -get)`
-      notify-send -u low "backlight" "decreased to $level"
+      level=`printf "%0.0f" $(light -G)`
+      notify low computer-laptop brightness $level
       ;;
 esac
