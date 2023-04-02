172d34f872a9e3930d8aa77ce1aa6ac36201d6e8
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-04-02 17:07:42 +1000

    bump nvm-sh version to v0.39.3

--- .local/bin/install-nvm.sh
+++ .local/bin/install-nvm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export NVM_DIR="$XDG_DATA_HOME/nvm"
 export NVM_BIN="$XDG_DATA_HOME/node"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
