2706c23fc2a5f4aee4c1f5fc6cfb85114d69913e
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-05-08 10:58:59 +1000

    add elan touchpad

--- .local/bin/touchpad.sh
+++ .local/bin/touchpad.sh
@@ -1,4 +1,9 @@
-#!/bin/env zsh
+#!/bin/sh
 
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "Elan Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "Elan Touchpad" "libinput Natural Scrolling Enabled" 1
+# xinput set-prop 11 "libinput Tapping Enabled" 1
+# xinput set-prop 11 "libinput Natural Scrolling Enabled" 1
+
