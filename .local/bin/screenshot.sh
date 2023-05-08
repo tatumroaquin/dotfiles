cc12b6c80c4cabcda24a1001ae17c6e400a0f54b
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-05-08 22:32:36 +1000

    add window screenshot functionality

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -2,15 +2,22 @@
 
 param=$1
 
+SCREENSHOTS="$HOME/Pictures/screenshots/"
+
+[[ ! -d "$SCREENSHOTS" ]] && mkdir -p "$SCREENSHOTS"
+
+filename=`date +"%Y-%m-%d_%H.%M.%S"`
+
 case $param in 
    screen)
-      filename=`date +"%Y-%m-%d_%H.%M.%S"`
-      maim ~/Pictures/screenshots/$filename.png
+      maim $SCREENSHOTS/$filename.png
       notify-send -u low "Desktop Screenshot" "$filename.png"
       ;;
    partial)
-      filename=`date +"%Y-%m-%d_%H.%M.%S"`
-      maim -s ~/Pictures/screenshots/$filename.png
+      maim -s $SCREENSHOTS/$filename.png
       notify-send -u low "Partial Screenshot" "$filename.png"
       ;;
+    window)
+      maim -i $(xdotool getactivewindow) $SCREENSHOTS/$filename.png
+      notify-send -u low "Window Screenshot" "$filename.png"
 esac
