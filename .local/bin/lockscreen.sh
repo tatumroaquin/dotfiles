4cced8d003d14f5620ec691ef9c97af7a12009e2
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2024-02-02 11:46:23 +1100

    add styling to lockscreen.sh script

--- .local/bin/lockscreen.sh
+++ .local/bin/lockscreen.sh
@@ -8,4 +8,17 @@ tmpbg='/tmp/screen.png'
 maim "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock --nofork -u -i "$tmpbg"
+i3lock \
+  --nofork \
+  -e -i "$tmpbg" \
+  --ring-width="20" \
+  --line-color="00ff00dd" \
+  --verif-text="verify" \
+  --verif-font="Hack Nerd Font Propo" \
+  --verif-font="12" \
+  --wrong-text="wrong" \
+  --wrong-font="Hack Nerd Font Propo" \
+  --wrong-font="12" \
+  --insidever-color="fa9c1bbb" \
+  --ringver-color="fa9c1bff" \
+  --modif-color="0000ffff"
