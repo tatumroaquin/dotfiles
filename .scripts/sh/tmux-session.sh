24977d97fc74ebf836cc6ab62b6a1af47920ad09
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2024-04-28 20:11:41 +1000

    run formatter on tmux scripts

--- .scripts/sh/tmux-session.sh
+++ .scripts/sh/tmux-session.sh
@@ -2,36 +2,36 @@
 
 # https://raw.githubusercontent.com/ThePrimeagen/.dotfiles/master/bin/.local/scripts/tmux-sessionizer
 
-dirlist=( ~/Code ~/Obsidian ~/Documents ~/.config )
+dirlist=(~/Code ~/Obsidian ~/Documents ~/.config)
 
 get_dir() {
-  for dir in "${dirlist[@]}"; do
-    find "$dir" -mindepth 1 -maxdepth 1 -type d
-  done | fzf-tmux -p 50%,100%
+	for dir in "${dirlist[@]}"; do
+		find "$dir" -mindepth 1 -maxdepth 1 -type d
+	done | fzf-tmux -p 50%,100%
 }
 
 # if args is equal to 1
 if [ $# -eq 1 ]; then
-  session_dir=$1
+	session_dir=$1
 else
-  session_dir=$( get_dir )
+	session_dir=$(get_dir)
 fi
 
 # if $session_dir string is empty exit no errors
 if [ -z "$session_dir" ]; then
-  exit 0
+	exit 0
 fi
 
 session_name=$(basename "$session_dir" | tr . _)
 
 # tmux is running
 if [ -z "$TMUX" ]; then
-  tmux new-session -c "$session_dir" -s "$session_name"
+	tmux new-session -c "$session_dir" -s "$session_name"
 fi
 
 # tmux has no session named $session_name create one
-if ! tmux has-session -t "$session_name" 2> /dev/null; then
-  tmux new-session -c "$session_dir" -ds "$session_name"
+if ! tmux has-session -t "$session_name" 2>/dev/null; then
+	tmux new-session -c "$session_dir" -ds "$session_name"
 fi
 
 # send signal to switch client to $session_name
--- .scripts/sh/tmux-window.sh
+++ .scripts/sh/tmux-window.sh
@@ -3,13 +3,13 @@
 # https://dev.to/pratham/tmux-fast-window-switching-across-sessions-3n0c
 
 if [ $# -eq 1 ]; then
-  window="$1"
+	window="$1"
 else
-  window="$(tmux list-windows -a -F "#S:#I-#W"| fzf-tmux -p)"
+	window="$(tmux list-windows -a -F "#S:#I-#W" | fzf-tmux -p)"
 fi
 
 if [ -z "$window" ]; then
-  exit 0
+	exit 0
 fi
 
 target=${window%-*}
