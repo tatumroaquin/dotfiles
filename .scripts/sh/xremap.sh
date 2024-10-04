1cd3aa81538a3f0cab3f93363c8f4fee645d8bc4
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2024-10-04 23:05:51 +1000

    feat(xremap): autogen xremap config when not existing

--- .scripts/sh/xremap.sh
+++ .scripts/sh/xremap.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if [ ! -f '/etc/udev/rules.d/input.rules' ]; then
+    sudo gpasswd -a $USER input
+    echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee '/etc/udev/rules.d/input.rules'
+fi
+
+if [ ! -f '/etc/modules-load.d/uinput.conf' ]; then
+    echo 'uinput' | sudo tee '/etc/modules-load.d/uinput.conf'
+fi
+
 killall -wq xremap
 
-xremap --watch $XDG_CONFIG_HOME/xremap/config.yml
+xremap --watch "$XDG_CONFIG_HOME/xremap/config.yml"
