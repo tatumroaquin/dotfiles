33acd796891237d2240c877692a9f1a10714d342
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2024-12-25 12:21:15 +1100

    fix: Wallpaper random option missing image path
    - `swww` needs path to set wallpaper for wayland
    - use `ls` and `shuf` to select random wallpaper
    - join `$walls` to `$image` in random and stills
    - retab the entire file to organise indentation

--- .scripts/sh/wallpaper.sh
+++ .scripts/sh/wallpaper.sh
@@ -2,10 +2,10 @@
 
 # kill previous instances of this script before running
 # $$ refers to the current process which is $0
-for pid in `pgrep -f $0`; do
-   if [[ $pid != $$ ]]; then
-      kill $pid
-   fi
+for pid in $(pgrep -f $0); do
+  if [[ $pid != $$ ]]; then
+    kill $pid
+  fi
 done
 
 state=$1
@@ -14,48 +14,49 @@ error_msg="Usage: $0 random, $0 slide <seconds> or $0 still <wallpaper.png>"
 alpha="90" # https://www.reddit.com/r/tmux/comments/nrjtuh/comment/h0gygzy/
 
 case $state in
-   random)
-      # feh -r --bg-fill --randomize --no-fehbg $walls
+  random)
+    # feh -r --bg-fill --randomize --no-fehbg $walls
+    image=$(ls -1 "$walls" | shuf -n 1)
+    if [[ -n $WAYLAND_DISPLAY ]]; then
+      swww img "$walls/$image"
+    fi
+    wal -i "$walls/$image" -a "$alpha"
+    ;;
+
+  still)
+    if [[ -n "$2" ]]; then
+      image=$2
+      # feh --bg-fill --no-fehbg $walls/$image
       if [[ -n $WAYLAND_DISPLAY ]]; then
-        swww img $image
+        swww img "$walls/$image"
       fi
-      wal -i "$walls" -a "$alpha"
-   ;;
+      wal -i "$walls/$image" -a "$alpha"
+    else
+      echo $error_msg
+    fi
+    ;;
 
-   still)
-         if [[ -n "$2" ]]; then 
-            image=$2
-            # feh --bg-fill --no-fehbg $walls/$image
-            if [[ -n $WAYLAND_DISPLAY ]]; then
-              swww img $image
-            fi
-            wal -i "$walls/$image" -a "$alpha"
-         else
-            echo $error_msg
-         fi
-   ;;
+  slide)
+    if [[ -n "$2" ]]; then
+      time=$2
 
-   slide)
-      if [[ -n "$2" ]]; then 
-         time=$2
+      while true; do
+        walls=$(find "$HOME/Pictures/wallpapers" -type f | shuf)
+        while read w; do
+          # feh --no-fehbg --bg-fill "$w"
+          if [[ -n $WAYLAND_DISPLAY ]]; then
+            swww img "$w"
+          fi
+          wal -i "$w" -a "$alpha"
+          sleep $time
+        done < <(echo $walls)
+      done
+    else
+      echo $error_msg
+    fi
+    ;;
 
-         while true; do
-            walls=`find "$HOME/Pictures/wallpaper" -type f | shuf`
-            while read w; do
-               # feh --no-fehbg --bg-fill "$w"
-               if [[ -n $WAYLAND_DISPLAY ]]; then
-                 swww img $w
-               fi
-               wal -i "$w" -a "$alpha"
-               sleep $time
-            done < <(echo $walls)
-         done
-      else 
-         echo $error_msg
-      fi 
-   ;;
-
-   *)
-         echo $error_msg
-   ;;
+  *)
+    echo $error_msg
+    ;;
 esac
